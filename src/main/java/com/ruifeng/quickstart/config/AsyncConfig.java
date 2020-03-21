443d9c8de306aeac8d9aba2d15255fc4d12aa2e3
ruifeng.wu <ruifeng.wu@thoughtworks.com>
2020-03-21 14:17:00 +0800

    async config

---
+++ src/main/java/com/ruifeng/quickstart/config/AsyncConfig.java
@@ -0,0 +1,51 @@
+package com.ruifeng.quickstart.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+@EnableAsync
+@Slf4j
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(100);
+        executor.setQueueCapacity(100);
+        executor.setKeepAliveSeconds(200);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(60 * 10);
+        executor.setThreadNamePrefix("AsyncWorker-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new AsyncExceptionHandler();
+    }
+
+    private static class AsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+        @Override
+        public void handleUncaughtException(Throwable ex, Method method, Object... params) {
+            log.error("[AsyncExceptionHandler] Async method has uncaught exception, " + ex +
+                    "current Thread" + Thread.currentThread().getName() +
+                    "Caller " + method.getDeclaringClass() + "Method: " + method.getName() +
+                    "Params: " + String.join(", ", Arrays.toString(params))
+            );
+        }
+    }
+}
+
